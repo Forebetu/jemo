d1cd87a3c53d68afd0902ec2e735976588a8bbbf
Jinsheng Zhao <zhaojinsheng510@outlook.com>
2019-09-03 16:28:35 +0800

    init jdmo

---
+++ src/main/java/com/zjs/jemo/JemoApplication.java
@@ -0,0 +1,13 @@
+package com.zjs.jemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JemoApplication.class, args);
+	}
+
+}
