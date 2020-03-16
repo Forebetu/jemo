30b51b1069a7a0a5f7078698e2ca9b7176b2def6
Jinsheng Zhao <zhaojinsheng510@outlook.com>
2020-03-16 12:04:54 +0800

    jenkins test

--- src/main/java/com/zjs/jemo/JemoApplication.java
+++ src/main/java/com/zjs/jemo/JemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("jdemo start...");
 		SpringApplication.run(JemoApplication.class, args);
 	}
 
